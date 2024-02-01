8150b5631f3c0de3b77a63ba115fba56e77af563
wasishah <wasis7817@gmail.com>
2024-02-01 17:24:08 +0500

    this is modified code

---
+++ src/assets/Components/Home/home.jsx
@@ -0,0 +1,16 @@
+import './home.css'
+
+function Home(){
+
+    return( 
+    <>
+    <div className='react'>
+        <p>wassi shah React frontend</p>
+    </div>
+    
+    
+    </>
+      
+    )
+}
+export default Home;
