82694909cf6ce852918ad0319be23ba3b66bc8b1
wasishah <wasis7817@gmail.com>
2024-02-01 17:54:30 +0500

    my new branch

--- src/assets/Components/Home/home.jsx
+++ src/assets/Components/Home/home.jsx
@@ -5,7 +5,7 @@ function Home(){
     return( 
     <>
     <div className='react'>
-        <p>wassi shah React frontend</p>
+        <p>wassi shah React frontend</p>ghfgdfgdf gfdgdfgdf
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque ut illum officia est dolor in accusantium voluptatum quam. Debitis iusto nulla sit id natus ipsam commodi ipsum laudantium, in quaerat?</p>
     </div>
     
